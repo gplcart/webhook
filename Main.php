76e7668f5820b25a50c9006d4ee10037b8ba8fc5
GplCart <gplcart.software@gmail.com>
2018-01-02 16:37:55 +0200

    Rename main module class

--- Main.php
+++ Main.php
@@ -9,13 +9,13 @@
 
 namespace gplcart\modules\webhook;
 
-use gplcart\core\Container,
-    gplcart\core\Module as CoreModule;
+use gplcart\core\Module,
+    gplcart\core\Container;
 
 /**
  * Main class for Web Hook module
  */
-class Module
+class Main
 {
 
     /**
@@ -25,9 +25,9 @@ class Module
     protected $module;
 
     /**
-     * @param CoreModule $module
+     * @param Module $module
      */
-    public function __construct(CoreModule $module)
+    public function __construct(Module $module)
     {
         $this->module = $module;
     }
